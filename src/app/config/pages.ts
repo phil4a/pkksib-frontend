3f12cc2d6aadfafb34070a394e4a4ff8476031af
Dimitri <philjke@mail.ru>
2025-08-07 23:00:42 +0700

    initial commit

---
+++ src/app/config/pages.ts
@@ -0,0 +1,23 @@
+class Page {
+	HOME = '/';
+
+	MY_CHANNEL = '/my-channel';
+	SUBSCRIPTIONS = '/subscriptions';
+	HISTORY = '/history';
+	LINKED_VIDEOS = '/linked-videos';
+
+	FEEDBACK = '/feedback';
+
+	// VIDEO(path: string) {
+	// 	return `/v/${path}`;
+	// }
+
+	// CHANNEL(path: string) {
+	// 	return `/c/${path}`;
+	// }
+
+	// SEARCH(searchTerm: string) {
+	// 	return `/s?term=${searchTerm}`;
+	// }
+}
+export const PAGE = new Page();
