9ec7cc0293929c2305b1da601bbbcce80ca51d69
phil <philjke@mail.ru>
2025-09-01 22:14:55 +0700

    feat(home-page): add how-we-work section and restructure hero components
    - Create new HowWeWork component for displaying company workflow
    - Move HeroBullet component to content/hero directory
    - Update page layout to include new section

---
+++ src/components/layout/content/how-we-work/HowWeWork.tsx
@@ -0,0 +1,14 @@
+export function HowWeWork() {
+	return (
+		<section className='bg-light-gray'>
+			<div className='layout-container py-25'>
+				<div className=''>
+					<h2 className='text-4xl font-semibold mb-3'>Как мы работаем</h2>
+					<p className='text-dark-gray'>
+						Основные этапы выполнения работ в «Первой Кровельной Компании»
+					</p>
+				</div>
+			</div>
+		</section>
+	);
+}
