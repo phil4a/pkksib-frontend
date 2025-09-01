ceb9c409bb9e22012b0d5a54c0d668b05a0e88f9
phil <philjke@mail.ru>
2025-09-01 15:55:40 +0700

    feat(home-page): enhance hero section with new roof image and content
    - Add new roof image asset for home page hero section
    - Implement tailwind-clamp plugin for responsive typography
    - Extend Button component to support links and custom classes
    - Update home page layout with additional content section and improved styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,50 +3,91 @@ import Image from 'next/image';
 import { Button } from '@/ui/button/Button';
 
 import mainBgImage from '@/assets/home-page/hero/main-bg.jpg';
+import roofImage from '@/assets/home-page/hero/roof.png';
 
 export default function HomePage() {
 	return (
-		<div className='layout-container'>
-			<div className='flex gap-5 h-[500px]'>
-				<div className='flex-1/2 flex flex-col justify-between items-start bg-accent rounded-xl pt-16 pb-12 px-12 relative'>
-					<div>
-						<h1 className='text-4xl font-bold mb-6 leading-[1.15] max-w-[366px]'>
-							Кровельные работы и материалы
-						</h1>
-						<p className='text-lg'>
-							Оптовые цены на кровлю и фасад, прозрачный расчёт — экономия до 20%
-						</p>
+		<section>
+			<div className='layout-container'>
+				<div className='flex gap-5 h-[500px] mb-4'>
+					<div className='flex-1/2 flex flex-col justify-between items-start bg-accent rounded-xl pt-16 pb-12 px-12 relative'>
+						<div>
+							<h1 className='text-4xl font-bold mb-6 leading-[1.15] max-w-[366px] z-1'>
+								Кровельные работы и материалы
+							</h1>
+							<p className='text-lg z-1'>
+								Оптовые цены на кровлю и фасад, прозрачный расчёт — экономия до 20%
+							</p>
+						</div>
+						<Button
+							type='black'
+							className='z-1'
+						>
+							Получить консультацию
+						</Button>
+						<svg
+							width='340'
+							height='212'
+							viewBox='0 0 340 212'
+							fill='none'
+							xmlns='http://www.w3.org/2000/svg'
+							className='absolute bottom-0 right-0'
+						>
+							<path
+								d='M296.119 27.4795C301.942 20.7868 312.124 20.7868 317.948 27.4795L526.235 266.863C534.685 276.575 527.987 292 515.32 292H98.7461C86.0791 292 79.3814 276.575 87.8316 266.863L296.119 27.4795Z'
+								fill='white'
+								fillOpacity='0.15'
+							/>
+							<path
+								d='M197.348 4.78846C203.206 -1.59615 213.485 -1.59615 219.343 4.78846L268.885 58.7784L111.565 239.59H14.7597C2.04037 239.59 -4.70013 224.987 3.76203 215.765L197.348 4.78846Z'
+								fill='white'
+								fillOpacity='0.1'
+							/>
+						</svg>
 					</div>
-					<Button type='black'>Получить консультацию</Button>
-					<svg
-						width='340'
-						height='212'
-						viewBox='0 0 340 212'
-						fill='none'
-						xmlns='http://www.w3.org/2000/svg'
-						className='absolute bottom-0 right-0'
-					>
-						<path
-							d='M296.119 27.4795C301.942 20.7868 312.124 20.7868 317.948 27.4795L526.235 266.863C534.685 276.575 527.987 292 515.32 292H98.7461C86.0791 292 79.3814 276.575 87.8316 266.863L296.119 27.4795Z'
-							fill='white'
-							fillOpacity='0.15'
-						/>
-						<path
-							d='M197.348 4.78846C203.206 -1.59615 213.485 -1.59615 219.343 4.78846L268.885 58.7784L111.565 239.59H14.7597C2.04037 239.59 -4.70013 224.987 3.76203 215.765L197.348 4.78846Z'
-							fill='white'
-							fillOpacity='0.1'
-						/>
-					</svg>
+					<div className='flex-1/2 relative'>
+						<Image
+							src={mainBgImage}
+							alt={'Первая кровельная компания'}
+							className='object-cover rounded-xl'
+							priority={true}
+							draggable={false}
+							fill={true}
+						></Image>
+					</div>
 				</div>
-				<div className='flex-1/2'>
-					<Image
-						src={mainBgImage}
-						alt={'Первая кровельная компания'}
-						priority={true}
-						draggable={false}
-					></Image>
+				<div className='flex gap-5 items-end bg-light-gray rounded-xl'>
+					<div className='flex-5/12'>
+						<Image
+							src={roofImage}
+							alt='кровля дома'
+							priority={true}
+							className='object-cover'
+							draggable={false}
+						></Image>
+					</div>
+					<div className='flex-7/12 flex flex-col py-15 items-start justify-center'>
+						<div className='clamp-[pr,5,20]'>
+							<h4 className='mb-4 text-[22px] font-semibold leading-[1.25]'>
+								В «Первой Кровельной Компании» помимо товаров для кровли европейского качества
+								предоставляется ряд услуг. У нас имеются мастера, которые выполнят монтажные работы
+								в Новосибирске и области.
+							</h4>
+							<p className='mb-6'>
+								Высокая квалификация и большой опыт работы позволяет выполнять установку кровельных
+								и фасадных материалов быстро и качественно. При этом соблюдаются все правила
+								выполнения такого рода строительных работ.
+							</p>
+						</div>
+						<Button
+							type={'accent'}
+							href='/about'
+						>
+							Узнать больше
+						</Button>
+					</div>
 				</div>
 			</div>
-		</div>
+		</section>
 	);
 }
--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -1,16 +1,27 @@
+import Link from 'next/link';
+
 interface Props {
+	href?: string;
 	children: React.ReactNode;
 	type: 'accent' | 'black';
+	className?: string;
 }
 
-export function Button({ children, type }: Props) {
-	return (
-		<button
-			className={`bg-accent hover:brightness-95 font-semibold h-12 px-6 rounded-lg cursor-pointer transition ${
-				type === 'black' ? 'bg-primary text-white' : ''
-			}`}
-		>
-			{children}
-		</button>
-	);
+export function Button({ children, className, type, href }: Props) {
+	const styles = `inline-flex items-center bg-accent hover:brightness-95 font-semibold h-12 px-6 rounded-lg cursor-pointer transition ${
+		type === 'black' ? 'bg-primary text-white' : ''
+	} ${className}`;
+
+	if (href) {
+		return (
+			<Link
+				href={href}
+				className={styles}
+			>
+				{children}
+			</Link>
+		);
+	}
+
+	return <button className={styles}>{children}</button>;
 }
