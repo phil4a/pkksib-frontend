c3951f7a3e038aeaff6b9c626dc03f7996209878
phil <philjke@mail.ru>
2025-10-03 21:40:04 +0700

    feat(about): add hero section with breadcrumbs and content
    Implement hero section for about page including breadcrumbs, title, descriptive text and image. Update metadata with more detailed information about the company.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,14 +1,17 @@
 import type { Metadata } from 'next';
 
+import { Hero } from '@/components/layout/content/about/Hero';
+
 export const metadata: Metadata = {
-	title: 'О нас',
-	description: 'О нас ПКК'
+	title: 'Первая Кровельная Компания - Кровельные работы в Новосибирске',
+	description:
+		'В «Первой Кровельной Компании» помимо товаров для кровли европейского качества предоставляется ряд услуг. У нас имеются мастера, которые выполнят монтажные работы в Новосибирске и области.'
 };
 
 export default function AboutPage() {
 	return (
-		<div>
-			<h1>О нас</h1>
-		</div>
+		<>
+			<Hero />
+		</>
 	);
 }
---
+++ src/components/layout/content/about/Hero.tsx
@@ -0,0 +1,53 @@
+import Image from 'next/image';
+
+import { Breadcrumbs } from '@/ui/Breadcrumbs';
+import { Title } from '@/ui/title/Title';
+
+import aboutHero from '@/assets/about-page/hero-bg.png';
+
+export function Hero() {
+	return (
+		<section className='bg-light-gray'>
+			<div className='layout-container pt-16 flex items-end'>
+				<div className='max-w-[50%]'>
+					<Breadcrumbs
+						items={[
+							{ label: 'Главная', href: '/' },
+							{ label: 'О нас', isCurrent: true }
+						]}
+					/>
+					<Title
+						type='h1'
+						className='text-5xl font-semibold leading-[1.15] mt-3 mb-6'
+					>
+						О Первой кровельной компании
+					</Title>
+					<p className='text-dark-gray mb-4'>
+						В «Первой Кровельной Компании» помимо товаров для кровли европейского качества
+						предоставляется ряд услуг. У нас имеются мастера, которые выполнят монтажные работы в
+						Новосибирске и области.
+					</p>
+					<p className='text-dark-gray'>
+						Высокая квалификация и большой опыт работы позволяет выполнять установку кровельных и
+						фасадных материалов быстро и качественно. При этом соблюдаются все правила выполнения
+						такого рода строительных работ.
+					</p>
+					<ul className='mt-10 mb-12 list-disc font-semibold pl-5 marker:text-accent marker:text-xl'>
+						<li>
+							Если вы еще думаете, где заказать монтажные работы в Новосибирске, то считайте, что
+							уже нашли решение!
+						</li>
+					</ul>
+				</div>
+				<div className='relative basis-[50%] aspect-[16/9]'>
+					<Image
+						priority={true}
+						src={aboutHero}
+						alt='О нас'
+						fill
+					/>
+				</div>
+			</div>
+		</section>
+	);
+}
