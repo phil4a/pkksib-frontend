dfb8fef70d952560342bf1391999a3f988f1be3b
phil <philjke@mail.ru>
2025-10-19 12:33:48 +0700

    feat(about): improve responsive design for company numbers and values sections
    Update padding, margins, font sizes and grid layouts to better adapt to different screen sizes
    Add mobile-first responsive classes for better user experience on all devices

--- src/components/layout/content/about/CompanyNumbers.tsx
+++ src/components/layout/content/about/CompanyNumbers.tsx
@@ -6,7 +6,7 @@ import aboutNumbersBg from '@/assets/about-page/numbers-bg.jpg';
 
 export function CompanyNumbers() {
 	return (
-		<section className='relative pt-20 pb-10'>
+		<section className='relative pt-16 md:pt-20 pb-4 md:pb-10'>
 			<div className='absolute top-0 right-0 w-full h-full bg-black/50'>
 				<Image
 					src={aboutNumbersBg}
@@ -15,10 +15,10 @@ export function CompanyNumbers() {
 				/>
 			</div>
 			<div className='layout-container relative text-white'>
-				<div className='mb-16'>
+				<div className='mb-8 md:mb-16'>
 					<Title
 						type='h2'
-						className='max-w-94 mb-8'
+						className='max-w-62 md:max-w-94 mb-8 leading-[1.15]'
 					>
 						Компания в цифрах за 2025 год
 					</Title>
@@ -39,23 +39,23 @@ export function CompanyNumbers() {
 						</p>
 					</div>
 				</div>
-				<ul className='grid grid-cols-4 gap-5'>
+				<ul className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 sm:gap-4 gap-5'>
 					<li className='p-6 pt-5 bg-white/20 border-1 border-white/20 rounded-xl backdrop-blur-sm'>
-						<p className='text-4xl font-semibold mb-2'>10000 м²</p>
+						<p className='text-[26px] md:text-4xl font-semibold mb-2'>10000 м²</p>
 						<p className='text-light-gray'>
 							кровельных работ, из них по реконструкции более 5000 м²
 						</p>
 					</li>
 					<li className='p-6 pt-5 bg-white/20 border-1 border-white/20 rounded-xl backdrop-blur-sm'>
-						<p className='text-4xl font-semibold mb-2'>5000 м³</p>
+						<p className='text-[26px] md:text-4xl font-semibold mb-2'>5000 м³</p>
 						<p className='text-light-gray'>смонтированных домов из клееного и профильного бруса</p>
 					</li>
 					<li className='p-6 pt-5 bg-white/20 border-1 border-white/20 rounded-xl backdrop-blur-sm'>
-						<p className='text-4xl font-semibold mb-2'>25000 м²</p>
+						<p className='text-[26px] md:text-4xl font-semibold mb-2'>25000 м²</p>
 						<p className='text-light-gray'>выполненных различных фасадных работ</p>
 					</li>
 					<li className='p-6 pt-5 bg-white/20 border-1 border-white/20 rounded-xl backdrop-blur-sm'>
-						<p className='text-4xl font-semibold mb-2'>300</p>
+						<p className='text-[26px] md:text-4xl font-semibold mb-2'>300</p>
 						<p className='text-light-gray'>выполненных различных фасадных работ</p>
 					</li>
 				</ul>
