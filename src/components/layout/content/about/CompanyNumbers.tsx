1bf7cd419f3117494ad40e3e713db4d3f2df97ce
phil <philjke@mail.ru>
2025-11-19 10:24:01 +0700

    feat(about): add new value and update styling in about components
    Add "комплектация под ключ" value to OurValues component and update text color to accent in CompanyNumbers

--- src/components/layout/content/about/CompanyNumbers.tsx
+++ src/components/layout/content/about/CompanyNumbers.tsx
@@ -18,7 +18,7 @@ export function CompanyNumbers() {
 				<div className='mb-8 md:mb-16'>
 					<Title
 						type='h2'
-						className='max-w-62 md:max-w-94 mb-8 leading-[1.15]'
+						className='max-w-62 md:max-w-94 mb-8 leading-[1.15] text-accent'
 					>
 						Компания в цифрах за 2025 год
 					</Title>
@@ -41,21 +41,21 @@ export function CompanyNumbers() {
 				</div>
 				<ul className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 sm:gap-4 gap-5'>
 					<li className='p-6 pt-5 bg-white/20 border-1 border-white/20 rounded-xl backdrop-blur-sm'>
-						<p className='text-[26px] md:text-4xl font-semibold mb-2'>10000 м²</p>
+						<p className='text-[26px] md:text-4xl font-semibold mb-2 text-accent'>10000 м²</p>
 						<p className='text-light-gray'>
 							кровельных работ, из них по реконструкции более 5000 м²
 						</p>
 					</li>
 					<li className='p-6 pt-5 bg-white/20 border-1 border-white/20 rounded-xl backdrop-blur-sm'>
-						<p className='text-[26px] md:text-4xl font-semibold mb-2'>5000 м³</p>
+						<p className='text-[26px] md:text-4xl font-semibold mb-2 text-accent'>5000 м³</p>
 						<p className='text-light-gray'>смонтированных домов из клееного и профильного бруса</p>
 					</li>
 					<li className='p-6 pt-5 bg-white/20 border-1 border-white/20 rounded-xl backdrop-blur-sm'>
-						<p className='text-[26px] md:text-4xl font-semibold mb-2'>25000 м²</p>
+						<p className='text-[26px] md:text-4xl font-semibold mb-2 text-accent'>25000 м²</p>
 						<p className='text-light-gray'>выполненных различных фасадных работ</p>
 					</li>
 					<li className='p-6 pt-5 bg-white/20 border-1 border-white/20 rounded-xl backdrop-blur-sm'>
-						<p className='text-[26px] md:text-4xl font-semibold mb-2'>300</p>
+						<p className='text-[26px] md:text-4xl font-semibold mb-2 text-accent'>300</p>
 						<p className='text-light-gray'>выполненных различных фасадных работ</p>
 					</li>
 				</ul>
--- src/components/layout/content/about/OurValues.tsx
+++ src/components/layout/content/about/OurValues.tsx
@@ -11,7 +11,7 @@ export function OurValues() {
 				Наши ценности
 			</Title>
 			<div className='relative z-1'>
-				<ul className='flex flex-col md:flex-row gap-4 md:gap-10'>
+				<ul className='flex flex-col lg:flex-row gap-4 md:gap-10'>
 					<li className='flex gap-3 items-center'>
 						<div className='shrink-0 flex items-center justify-center bg-primary h-9 w-9 rounded-lg'>
 							<svg
@@ -62,6 +62,23 @@ export function OurValues() {
 							</svg>
 						</div>
 						<p className='font-semibold'>инновационные материалы</p>
+					</li>
+					<li className='flex gap-3 items-center'>
+						<div className='shrink-0 flex items-center justify-center bg-primary h-9 w-9 rounded-lg'>
+							<svg
+								width='22'
+								height='20'
+								viewBox='0 0 22 20'
+								fill='none'
+								xmlns='http://www.w3.org/2000/svg'
+							>
+								<path
+									d='M5.71289 19.9998H2.71484C1.61027 19.9998 0.714844 19.1043 0.714844 17.9998V10.5125C0.714844 9.40788 1.61027 8.51245 2.71484 8.51245H5.71289V19.9998ZM13.6572 0.0300293C14.809 0.366086 15.5066 1.55407 15.1328 2.69409C13.9137 6.41063 12.4675 7.63605 12.4414 7.65796H18.2842C20.2179 7.65796 21.6466 9.46094 21.2051 11.3435L19.7178 17.6843C19.3996 19.0403 18.1897 19.9987 16.7969 19.9988H6.77734V8.53784C9.06853 4.75192 10.9745 4.39342 12.7402 0.4646C12.8979 0.114126 13.2883 -0.0775602 13.6572 0.0300293Z'
+									fill='#FCD200'
+								/>
+							</svg>
+						</div>
+						<p className='font-semibold'>комплектация под ключ</p>
 					</li>
 				</ul>
 			</div>
