288362b6a021b347ca78da8a99e9ba35461b3550
phil <philjke@mail.ru>
2025-11-20 15:41:49 +0700

    feat(home-page): update partners section with new assets and improved carousel
    - Replace partner logos with updated PNG and SVG files
    - Add new partner logos (tn, velux, tegola, factum)
    - Refactor Partners component with better image handling and carousel settings
    - Improve carousel responsiveness and animation
    - Update partner logo imports and file paths

--- src/components/layout/content/home-page/partners/Partners.tsx
+++ src/components/layout/content/home-page/partners/Partners.tsx
@@ -8,23 +8,23 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 import { Title } from '@/ui/title/Title';
 
-import kubov from '@/assets/home-page/partners/100-kubov.webp';
-import aquasystem from '@/assets/home-page/partners/aquasystem.webp';
+import kubov from '@/assets/home-page/partners/100-kubov.png';
+import aquasystem from '@/assets/home-page/partners/aquasystem.png';
 import berezka from '@/assets/home-page/partners/berezka.webp';
+import dorken from '@/assets/home-page/partners/doerken.png';
 import domProsto from '@/assets/home-page/partners/dom-prosto.webp';
-import dorken from '@/assets/home-page/partners/dorken.svg';
-import factum from '@/assets/home-page/partners/factum.webp';
-import fakro from '@/assets/home-page/partners/fakro.webp';
-import shinglas from '@/assets/home-page/partners/shinglas.webp';
+import factum from '@/assets/home-page/partners/factum.svg';
+import fakro from '@/assets/home-page/partners/fakro.png';
+import shinglas from '@/assets/home-page/partners/shinglas.png';
 import sibBiz from '@/assets/home-page/partners/sib-biz.webp';
-import tegola from '@/assets/home-page/partners/tegola.webp';
+import tegola from '@/assets/home-page/partners/tegola.svg';
 import tehSoyuz from '@/assets/home-page/partners/teh-soyuz.webp';
-import teplostroy from '@/assets/home-page/partners/teplostroy.webp';
-import tn from '@/assets/home-page/partners/tn.webp';
+import teplostroy from '@/assets/home-page/partners/teplostroy.png';
+import tn from '@/assets/home-page/partners/tn.svg';
 import tolmachevo from '@/assets/home-page/partners/tolmachevo.webp';
-import tyvek from '@/assets/home-page/partners/tyvek.webp';
-import velux from '@/assets/home-page/partners/velux.webp';
-import zelLug from '@/assets/home-page/partners/zel-lug.webp';
+import tyvek from '@/assets/home-page/partners/tyvek.png';
+import velux from '@/assets/home-page/partners/velux.svg';
+import zelLug from '@/assets/home-page/partners/zel-lug.png';
 
 export function Partners() {
 	return (
@@ -40,17 +40,22 @@ export function Partners() {
 				spaceBetween={50}
 				slidesPerView={5}
 				loop={true}
+				autoHeight={false}
+				height={120}
+				allowTouchMove={false}
+				effect='slide'
+				speed={4000}
 				autoplay={{
-					delay: 3000,
-					disableOnInteraction: true
+					delay: 0
 				}}
+				wrapperClass='partners-wrapper'
 				breakpoints={{
 					320: {
-						slidesPerView: 1.1,
+						slidesPerView: 2,
 						spaceBetween: 16
 					},
 					560: {
-						slidesPerView: 2.1,
+						slidesPerView: 3,
 						spaceBetween: 16
 					},
 					768: {
@@ -63,101 +68,209 @@ export function Partners() {
 					}
 				}}
 			>
-				<SwiperSlide>
-					<Image
-						src={kubov}
-						alt='100 кубов'
-					/>
+				<SwiperSlide className='relative self-center'>
+					<div className='h-25'>
+						<Image
+							draggable={false}
+							className='w-full h-full object-contain'
+							height={100}
+							width={222}
+							src={kubov}
+							alt='100 кубов'
+						/>
+					</div>
 				</SwiperSlide>
-				<SwiperSlide>
-					<Image
-						src={aquasystem}
-						alt='Aquasystem'
-					/>
+				<SwiperSlide className='relative self-center '>
+					<div className='h-25'>
+						<Image
+							draggable={false}
+							className='w-full h-full object-contain'
+							height={100}
+							width={222}
+							src={aquasystem}
+							alt='Aquasystem'
+						/>
+					</div>
 				</SwiperSlide>
-				<SwiperSlide>
-					<Image
-						src={berezka}
-						alt='Березка'
-					/>
+				<SwiperSlide className='relative self-center'>
+					<div className='h-25'>
+						<Image
+							draggable={false}
+							className='w-full h-full object-contain'
+							height={100}
+							width={222}
+							src={berezka}
+							alt='Березка'
+						/>
+					</div>
 				</SwiperSlide>
-				{/* <SwiperSlide>
-					<Image
-						src={dorken}
-						alt='Доркен'
-					/>
-				</SwiperSlide> */}
-				<SwiperSlide>
-					<Image
-						src={factum}
-						alt='Фактум'
-					/>
+				<SwiperSlide className='relative self-center'>
+					<div className='h-25'>
+						<Image
+							draggable={false}
+							className='w-full h-full object-contain'
+							height={100}
+							width={222}
+							src={domProsto}
+							alt='Березка'
+						/>
+					</div>
 				</SwiperSlide>
-				{/* <SwiperSlide>
-					<Image
-						src={fakro}
-						alt='Факро'
-					/>
-				</SwiperSlide> */}
-				<SwiperSlide>
-					<Image
-						src={shinglas}
-						alt='Шинглас'
-					/>
+				<SwiperSlide className='relative self-center'>
+					<div className='h-25'>
+						<Image
+							draggable={false}
+							className='w-full h-full object-contain'
+							height={100}
+							width={222}
+							src={dorken}
+							alt='Дёркен'
+						/>
+					</div>
 				</SwiperSlide>
-				<SwiperSlide>
-					<Image
-						src={sibBiz}
-						alt='SibBiz'
-					/>
+				<SwiperSlide className='relative self-center'>
+					<div className='h-25'>
+						<Image
+							draggable={false}
+							className='w-full h-full object-contain'
+							height={100}
+							width={222}
+							src={factum}
+							alt='Фактум'
+						/>
+					</div>
 				</SwiperSlide>
-				{/* <SwiperSlide>
-					<Image
-						src={tegola}
-						alt='Тегола'
-					/>
-				</SwiperSlide> */}
-				<SwiperSlide>
-					<Image
-						src={tehSoyuz}
-						alt='ТехСоюз'
-					/>
+				<SwiperSlide className='relative self-center'>
+					<div className='h-25'>
+						<Image
+							draggable={false}
+							className='w-full h-full object-contain'
+							height={100}
+							width={222}
+							src={fakro}
+							alt='Факро'
+						/>
+					</div>
 				</SwiperSlide>
-				<SwiperSlide>
-					<Image
-						src={teplostroy}
-						alt='Теплострой'
-					/>
+				<SwiperSlide className='relative self-center'>
+					<div className='h-25'>
+						<Image
+							draggable={false}
+							className='w-full h-full object-contain'
+							height={100}
+							width={222}
+							src={shinglas}
+							alt='Шинглас'
+						/>
+					</div>
 				</SwiperSlide>
-				<SwiperSlide>
-					<Image
-						src={tn}
-						alt='Технониколь'
-					/>
+				<SwiperSlide className='relative self-center'>
+					<div className='h-25'>
+						<Image
+							draggable={false}
+							className='w-full h-full object-contain'
+							height={100}
+							width={222}
+							src={sibBiz}
+							alt='SibBiz'
+						/>
+					</div>
 				</SwiperSlide>
-				<SwiperSlide>
-					<Image
-						src={tolmachevo}
-						alt='Толмачevo'
-					/>
+				<SwiperSlide className='relative self-center'>
+					<div className='h-25'>
+						<Image
+							draggable={false}
+							className='w-full h-full object-contain'
+							height={100}
+							width={222}
+							src={tegola}
+							alt='Тегола'
+						/>
+					</div>
 				</SwiperSlide>
-				<SwiperSlide>
-					<Image
-						src={tyvek}
-						alt='Tyvek'
-					/>
+				<SwiperSlide className='relative self-center'>
+					<div className='h-25'>
+						<Image
+							draggable={false}
+							className='w-full h-full object-contain'
+							height={100}
+							width={222}
+							src={tehSoyuz}
+							alt='ТехСоюз'
+						/>
+					</div>
 				</SwiperSlide>
-				<SwiperSlide>
-					<Image
-						src={velux}
-						alt='Velux'
-					/>
+				<SwiperSlide className='relative self-center'>
+					<div className='h-25'>
+						<Image
+							draggable={false}
+							className='w-full h-full object-contain'
+							height={100}
+							width={222}
+							src={teplostroy}
+							alt='Теплострой'
+						/>
+					</div>
 				</SwiperSlide>
-				<SwiperSlide>
-					<Image
-						src={zelLug}
-						alt='Зеленый луг'
-					/>
+				<SwiperSlide className='relative self-center'>
+					<div className='h-25'>
+						<Image
+							draggable={false}
+							className='w-full h-full object-contain'
+							height={100}
+							width={222}
+							src={tn}
+							alt='Технониколь'
+						/>
+					</div>
+				</SwiperSlide>
+				<SwiperSlide className='relative self-center'>
+					<div className='h-25'>
+						<Image
+							draggable={false}
+							className='w-full h-full object-contain'
+							height={100}
+							width={222}
+							src={tolmachevo}
+							alt='Толмачevo'
+						/>
+					</div>
+				</SwiperSlide>
+				<SwiperSlide className='relative self-center'>
+					<div className='h-25'>
+						<Image
+							draggable={false}
+							className='w-full h-full object-contain'
+							height={100}
+							width={222}
+							src={tyvek}
+							alt='Tyvek'
+						/>
+					</div>
+				</SwiperSlide>
+				<SwiperSlide className='relative self-center'>
+					<div className='h-25'>
+						<Image
+							draggable={false}
+							className='w-full h-full object-contain'
+							height={100}
+							width={222}
+							src={velux}
+							alt='Velux'
+						/>
+					</div>
+				</SwiperSlide>
+				<SwiperSlide className='relative self-center'>
+					<div className='h-25'>
+						<Image
+							draggable={false}
+							height={100}
+							width={222}
+							src={zelLug}
+							className='w-full h-full object-contain'
+							alt='Зеленый луг'
+						/>
+					</div>
 				</SwiperSlide>
 			</Swiper>
 		</div>
